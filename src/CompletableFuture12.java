57528241d4cd74f969a5fe2e6f41e7e70f1bb2c3
minhvu98 <zxboy2007@gmail.com>
2020-12-18 01:14:40 +0700

    CompletableFuture1

---
+++ src/CompletableFuture12.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFuture12 {
+	 public static void main(String[] args) throws InterruptedException, ExecutionException {
+	        CompletableFuture<String> maturityFuture = 
+	                CompletableFuture.supplyAsync(() -> -1).thenApply(age -> {
+	            if (age < 0) { throw new IllegalArgumentException("Age can not be negative"); } if (age > 18) {
+	                return "Adult";
+	            } else {
+	                return "Child";
+	            }
+	        }).exceptionally(ex -> {
+	            System.out.println("Oops! We have an exception - " + ex.getMessage());
+	            return "Unknown!";
+	        });
+	        System.out.println("Maturity : " + maturityFuture.get());
+	    }
+	}
